0b99b68475cf6cd09017c9e3041968481dc607d8
akashgupta1116 <yoakash6@gmail.com>
2026-01-15 23:48:58 +0530

    server implementation

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,10 +8,39 @@ const server = createServer(app);
 
 const port: number = 4600;
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
 
+const ROOM = 'group'
 io.on('connection', (socket) => {
-  console.log('connection is build');
+
+  socket.on('joinRoom', async (userName : string)=> {
+
+        await socket.join(ROOM)
+
+        // broadcast the notice
+        socket.to(ROOM).emit('roomNotice', userName)
+
+        
+  })
+
+  socket.on('chatMessage', (msg: any)=> {
+    socket.to(ROOM).emit('chatMessage', msg)
+  })
+
+  socket.on('typing', (userName: string)=> {
+
+      socket.to(ROOM).emit("typing", userName)
+  })
+
+  socket.on('stopTyping', (userName: string)=> {
+
+    socket.to(ROOM).emit("stopTyping", userName)
+})
+
 });
 
 // Define a route handler for the default home page
